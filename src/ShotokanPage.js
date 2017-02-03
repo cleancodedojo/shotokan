3498292633d71b726ffb4896adc31fe8ab7b5397
raydecastro <captray108@gmail.com>
2017-02-03 02:37:37 -0800

    feature. added jka logo.

---
+++ src/ShotokanPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ShotokanPage extends Component {
+    render() {
+        return (
+            <div className="shotokan-page">
+                shotokan page!
+            </div>
+        );
+    }
+}
+
+export default ShotokanPage;
