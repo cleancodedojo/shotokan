4b00a67429b98ed485848de46292d3651ccf4d1d
raydecastro <captray108@gmail.com>
2017-02-03 03:18:07 -0800

    feature. incorporated shotokan core to ui. added preliminary kata list render.

--- src/ShotokanPage.js
+++ src/ShotokanPage.js
@@ -1,12 +1,28 @@
 import React, { Component } from "react";
-
+import Shotokan from "shotokan-core";
 import "./ShotokanPage.css";
 
 class ShotokanPage extends Component {
+    constructor(props) {
+        super(props);
+
+        this.shotokan = new Shotokan.KataVideoSource();
+    }
+
     render() {
+        let kataList = this.shotokan.getJKAKatasFromYouTubeVideoList();
+        let katas = [];
+
+        for (let k = 0; k < kataList.length; k += 1) {
+            katas.push(<li key={k}>{kataList[k].kataName}</li>);
+        }
+
         return (
             <div className="shotokan-page">
                 <h2>Kata</h2>
+                <ul>
+                    {katas}
+                </ul>
             </div>
         );
     }
